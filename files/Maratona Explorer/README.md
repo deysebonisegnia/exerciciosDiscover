3d9827c8cd0a42594de06b33bfd3c079955ff6f4
deysebonisegnia <dbonisegniabf@gmail.com>
2022-10-05 15:02:30 -0300

    agrupandoOsExercicios

---
+++ files/Maratona Explorer/README.md
@@ -0,0 +1,6 @@
+Um jeito diferente de commit fácil,rápido e sem git.
+<i class="fa-brands fa-js-square"></i>
+<i class="fa-brands fa-html5"></i>
+<i class="fa-brands fa-css3-alt"></i>
+
+
---
+++ files/Mensagem/script.js
@@ -0,0 +1,31 @@
+
+
+
+    
+   
+function carConteudo(params) {
+    document.getElementById("conteudo").innerHTML =  
+    "<p> Conecte-se com bons pensamentos e sentimentos.Resgate a leveza da vida.</p> "
+    "<p> Veja a beleza da vida</p> "
+    
+
+}
+
+ function removerMensagem(params) {
+    setTimeout(function()  {
+        document.getElementById("conteudo").innerHTML =
+         "<img src='./img/paraiso1.webp'>"
+
+
+    }, 4000);
+    
+}
+    removerMensagem();
+
+    
+    
+
+
+
+
+
