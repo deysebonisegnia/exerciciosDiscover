3dd7fe1a0918d1156a81d886e109505da213e597
deysebonisegnia <dbonisegniabf@gmail.com>
2022-10-26 12:37:39 -0300

    exercicioReceita

--- files/exercicio 6/exer06.js
+++ files/exercicio 6/exer06.js
@@ -1,26 +1,28 @@
 // transformDegree('50f')
     function transformDegree(degree){
     const celsiusExists = degree.toUpperCase().includes('C')
-    const fahrenheitExists = degree.toUpperCase().includes('F')
+    const fahrenheitExists = degree.toUpperCase().includes('F') 
+
+
     if (!celsiusExists && !fahrenheitExists){
        throw new Error('Grau não identificado') 
     }
     let updatedDegree = Number(degree.toUppercase().replace("F","f"));
-    let formula = fahrenheit  => (fahrenheit - 32) * 5/9
+    let formula = (fahrenheit)  => (fahrenheit - 32) * 5/9
     let degreeSign = 'C'  
-    return formula(updatedDegree) + degreeSign
+   
 }
     if(celsiusExists){
         updatedDegree = Number(degree.toUppercase().replace("C","c"));
-         formula = celsius  => celsius * 9/5 + 32
+         formula = (celsius ) => (celsius * 9/5 )+ 32
          degreeSign = 'F'  
-        return formula(updatedDegree) + degreeSign 
+        
     }
 try{
     console.log(transformDegree('50C'))
    // transformDegree('50z')
 }catch (error){
-    console.log(error)
+    console.log(error.message)
 }
 
 
